3b222e215993879c7022df640a6ea03148160fd5
Ishar Maharjan <isharmaharjan123@gmail.com>
2022-05-09 02:27:51 +0545

    meow

--- django/api/utils.py
+++ django/api/utils.py
@@ -4,16 +4,47 @@ from keras.preprocessing import image
 from tensorflow import Graph
 import tensorflow as tf
 from django.conf import settings
+import cv2
 import os
 from rest_framework.response import Response
 import numpy as np
+from keras import backend as K
+from tensorflow.keras.optimizers import Adam
 
-# model_graph = Graph()
-# with model_graph.as_default():
-#     tf_session = tf.compat.v1.Session()
-#     with tf_session.as_default():
-#         animalmodel = load_model('./models/MobileNetModelImagenet.h5')
-        # similaritymodel = load_model("")
+def initialize_weights(shape, dtype=None):
+    return np.random.normal(loc = 0.0, scale = 1e-2, size = shape)
+
+def initialize_bias(shape, dtype=None):
+    return np.random.normal(loc = 0.5, scale = 1e-2, size = shape)
+
+def euclidean_distance(vectors):
+    (featsA, featsB) = vectors
+    
+    sumSquared = K.sum(K.square(featsA - featsB), axis=1,
+        keepdims=True)
+    
+    return K.sqrt(K.maximum(sumSquared, K.epsilon()))
+
+def contrastive_loss(y, preds, margin=0.6):
+    y = tf.cast(y, preds.dtype)
+    squaredPreds = K.square(preds)
+    squaredMargin = K.square(K.maximum(margin - preds, 0))
+    loss = K.mean(y * squaredPreds + (1 - y) * squaredMargin)
+    return loss
+
+
+model_graph = Graph()
+with model_graph.as_default():
+    tf_session = tf.compat.v1.Session()
+    with tf_session.as_default():
+        animalmodel = load_model('./models/MobileNetModelImagenet.h5')
+# siamese_model1 = tf.keras.models.load_model('./models/siamese_model2_h5.h5', compile=False)
+
+        siamese_model1 =  tf.keras.models.load_model('./models/saved_model_siamese', compile=False, custom_objects = {'euclidean_distance': euclidean_distance})
+        optimizer = Adam(learning_rate = 0.0001)
+        siamese_model1.compile(loss=contrastive_loss,optimizer=optimizer, metrics = ['accuracy'])
+        # siamese_model2.compile(loss=contrastive_loss,optimizer=optimizer, metrics = ['accuracy'])
+
 
 valid_animals = [
                 "antelope", "cockroach", "eagle", "hare", "lion", "oyster", "rhinoceros", "tiger",
@@ -70,9 +101,50 @@ def predict_similarity_scores(image):
     
     return scores, path
 
+def _similarity_score_logic(input1, input2):
+    if input1 > 57 and input2 > 57:
+        if input1 < 60 and input2 < 60:
+            score = input2 + randint(35,39)  
+        elif input1 > 60 and input2 > 60:
+            score = 100 - randint(0,10) - input1/input2
+        else:
+            score = 100 - randint(7,15) - input1/input2
+    elif input2 > 57:
+        score = 100 - randint(10,15) - input1/input2
+    elif int(input2) in range(40,58):
+        score = input2/65 * 100
+    elif int(input2) in range(30,40):
+        score = input2/60 * 100
+    elif input2 < 10:
+        score = input2 + 10
+    else:
+        score = input2
+    
+    return score
+
+def _process_images(p1, p2):
+    image_1 = cv2.imread(p1.name)
+    image_2 = cv2.imread(p2.path)
+    resized3=cv2.resize(image_1, (128,128),interpolation=cv2.INTER_AREA)
+    rescaled3=1.0/255*resized3
+    r1 = np.array((rescaled3))
+
+    resized2=cv2.resize(image_2, (128,128),interpolation=cv2.INTER_AREA)
+    rescaled2=1.0/255*resized2
+    r2 = np.array((rescaled2))
+
+    paired1 = np.array((r1,r1))
+    paired2 = np.array((r2,r2))
+
+    return paired1, paired2
+
 def calculate_similarities(anchor, *positives):
     result = []
     for p in positives:
-        r = similaritymodel.predict((p, anchor))
-        result.append(r[0][0], p.url)
-    return positives[0].url
+        p1, p2 = _process_images(anchor, p)
+        score1 = siamese_model1.predict([p1, p2])[0][0]
+        # score2 = siamese_model2.predict([p1, p2])[0][0]
+        # score = _similarity_score_logic(score1, score2)
+        result.append((p, score1))
+    print(result)
+    return result
